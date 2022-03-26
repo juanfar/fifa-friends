91d35addc3624e946cbae1d12320a479acd5d976
Juan Fajardo <juanfar.group@gmail.com>
2022-03-26 20:52:55 +0100

    feat: login template with styles

--- src/views/home.js
+++ src/views/home.js
@@ -6,7 +6,7 @@ export const homeView = `
     <div class="col-lg-6 mx-auto">
       <p class="lead mb-4">Entra o registrate para crear tu liga y competir con tus amigos!.</p>
       <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
-        <button type="button" class="btn btn-outline-primary btn-lg px-4 gap-3" onclick="window.location.hash = '#/login'">Entrar</button>
+        <button type="button" class="btn btn-outline-primary btn-lg px-4 gap-3" onclick="window.location.hash = '#/login'">Ingresar</button>
         <button type="button" class="btn btn-outline-success btn-lg px-4" onclick="window.location.hash = '#/register'">Registrarse</button>
       </div>
     </div>
--- src/views/login.js
+++ src/views/login.js
@@ -1,3 +1,24 @@
 export const loginView = `
-  <h2>Login Works!</h2>
+<main class="page__form">
+  <form>
+  <!-- <img class="mb-4" src="/docs/5.1/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57"> -->
+    <span class="brand">FL</span>
+    <div class="form-floating mt-4">
+      <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+      <label for="floatingInput">Correo Electrónico</label>
+    </div>
+    <div class="form-floating">
+      <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+      <label for="floatingPassword">Contraseña</label>
+    </div>
+
+    <!-- <div class="checkbox mb-3">
+      <label>
+        <input type="checkbox" value="remember-me"> Remember me
+      </label>
+    </div> -->
+    <button class="w-100 btn btn-lg btn-outline-primary mt-3" type="submit">Ingresar</button>
+    <p class="mt-5 mb-3 text-muted">&copy; juanfar - 2022</p>
+  </form>
+</main>
 `;
